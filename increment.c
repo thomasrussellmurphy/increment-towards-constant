0654d731d1d04f9184d5269d401109b1d56e601c
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2015-07-03 20:40:01 -0500

    Completed initial implementation for float in C

--- increment.c
+++ increment.c
@@ -3,22 +3,9 @@
 
 int main(int argc, char argv[]) {
   float incrementee = 0.0;
-  float historical_incrementee = 0.0;
-  int count = 0;
   
-  while((incrementee + 1) - incrementee > 0.999999999) {
+  while((incrementee + 1.0) - incrementee > 0.999999999) {
     incrementee += 1.0;
-    count++;
-    
-    if(count % 1000000 == 0) {
-      if (incrementee - historical_incrementee < 10.0) {
-        printf("breaking\n");
-        break;
-      }
-      printf("%f, %f\n", incrementee, (incrementee + 1.0) - incrementee);
-      count = 1;
-      historical_incrementee = incrementee;
-    }
   }
   
   printf("%f\n", incrementee);
