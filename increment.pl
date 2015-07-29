72a3774d6cd1fb46c8e1c59c5640afe157351223
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2015-07-28 22:25:28 -0500

    Is Perl being a wizard? I don't get it. . .

--- increment.pl
+++ increment.pl
@@ -1,17 +1,17 @@
 #!/usr/bin/env perl
 use POSIX ();
 
-my $incrementee = POSIX::pow(2, 53) - 10000.0;
+# With exactly the magic number, Perl exits
+# Drop a `- 1.0` on the end of the exponent and it'll just keep counting
+my $incrementee = POSIX::pow(2, 53); # - 1;
+
+printf("starting %f\n", ($incrementee + 1.0) - $incrementee);
 
 # Stop counting when we lose resolution of 1.0
 while ( ($incrementee + 1.0) - $incrementee > 0.99999999 )
 {
   $incrementee += 1.0;
-
-  if ($incrementee % 1000000 == 0)
-  {
-    printf("%f\n", $incrementee);
-  }
+  printf("in-loop %f\n", $incrementee);
 }
 
 printf "%f\n", $incrementee;
