f755a71367ebb21c50cb6d80b86638e62ff64e1e
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2015-07-28 22:11:02 -0500

    Still isn't working, have debug and better $variable-ing

--- increment.pl
+++ increment.pl
@@ -1,11 +1,17 @@
 #!/usr/bin/env perl
 use POSIX ();
 
-my $incrementee = POSIX::pow(2, 53) - 100000.0;
+my $incrementee = POSIX::pow(2, 53) - 10000.0;
 
 # Stop counting when we lose resolution of 1.0
-while ( (incrementee + 1.0) - incrementee > 0.99999999 ){
+while ( ($incrementee + 1.0) - $incrementee > 0.99999999 )
+{
   $incrementee += 1.0;
+
+  if ($incrementee % 1000000 == 0)
+  {
+    printf("%f\n", $incrementee);
+  }
 }
 
 printf "%f\n", $incrementee;
